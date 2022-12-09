6656f052253c99fe3d117c987c3e753bd4dda7f0
mixa1334 <micha252282@gmail.com>
2022-12-09 22:23:13 +0300

    feat: add organization search page

--- src/components/user/find-organization.component.js
+++ src/components/user/find-organization.component.js
@@ -1,15 +1,75 @@
 import React, { Component } from "react";
+import AuthService from "../../services/auth/auth.service";
+import OrgService from "../../services/organization/organization.service";
 
 export default class FindOrganization extends Component {
   constructor(props) {
     super(props);
+    this.getAllAvailableOrganizations = this.getAllAvailableOrganizations.bind(this);
+    this.sendRequestToJoinOrg = this.sendRequestToJoinOrg.bind(this);
+    this.onChangeNewOrgName = this.onChangeNewOrgName.bind(this);
+    this.onChangeNewOrgFunds = this.onChangeNewOrgFunds.bind(this);
+    this.createNewOrg = this.createNewOrg.bind(this);
 
+    this.state = {
+      organizations: [],
+      newOrgName: "",
+      newOrgFunds: 0
+    };
   }
 
   componentDidMount() {
+    this.getAllAvailableOrganizations();
+  }
+
+  getAllAvailableOrganizations(){
+    OrgService.getAllOrganizations()
+    .then(response => {
+      this.setState({
+        organizations: response.organizations
+      });
+    });
+  }
+
+  sendRequestToJoinOrg(orgId){
+    const userId = AuthService.getCurrentUser().userId;
+    OrgService.sendRequestToJoin(orgId, userId)
+    .then(()=>{
+      window.location.reload(false);
+    });
+  }
+
+  onChangeNewOrgName(e){
+    this.setState({
+      newOrgName: e.target.value
+    });
+  }
+
+  onChangeNewOrgFunds(e){
+    this.setState({
+      newOrgFunds: e.target.value
+    });
+  }
+
+  createNewOrg(){
+    const userId = AuthService.getCurrentUser().userId;
+    var data = {
+      userId: userId,
+      name: this.state.newOrgName,
+      funds: this.state.newOrgFunds
+    }
+    OrgService.createNewOrganization(data)
+    .then(()=>{
+      AuthService.updateUserInfo()
+      .then(()=>{
+        window.location = "/home";
+      });
+    });
   }
 
   render() {
+    const { organizations } = this.state;
+
     return (
       <>
       
@@ -17,14 +77,16 @@ export default class FindOrganization extends Component {
       <h1 className="head">Create new organization</h1>
       <div className="form-create">
         <label for="nameOrganization">Name</label>
-        <input type="text" className="form-control" id="nameOrganization"  placeholder="enter name"/>
+        <input type="text" className="form-control" id="nameOrganization"  placeholder="enter name"
+        required value={this.state.newOrgName} onChange={this.onChangeNewOrgName}/>
       </div>
       <div className="form-create">
         <label for="funds">Funds</label>
-        <input type="text" className="form-control" id="funds" placeholder="funds"/>
+        <input type="number" className="form-control" id="funds" placeholder="funds"
+        required value={this.state.newOrgFunds} onChange={this.onChangeNewOrgFunds}/>
       </div>
       <br/>
-      <button type="submit" className="btn btn-primary btn-lg">Create</button>
+      <button type="submit" className="btn btn-primary btn-lg" onClick={() => this.createNewOrg()}>Create</button>
     </div>
 
     <div className="container-table mt-3">
@@ -32,15 +94,18 @@ export default class FindOrganization extends Component {
       <table className="table table-borderless table-sm">
         <thead>
           <tr>
-            <th>Name organization</th>
+            <th>Organization name</th>
             <th></th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Food shop</td>
-            <td><button type="button" className="btn btn-primary">Send request</button></td>
-          </tr>
+
+          {organizations && organizations.map((org) => (
+            <tr>
+              <td>{org.name}</td>
+              <td><button type="button" className="btn btn-primary" onClick={() => this.sendRequestToJoinOrg(org.id)}>Send request</button></td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
--- src/services/organization/organization.service.js
+++ src/services/organization/organization.service.js
@@ -5,6 +5,29 @@ const API_URL = "http://localhost:8080/bps";
 
 class OrganizationService {
 
+    async getAllOrganizations(){
+        const response = await axios.get(API_URL + "/organizations", {
+            headers: authHeader()
+        });
+        return response.data;
+    }
+
+    async sendRequestToJoin(orgId, userId){
+        const response = await axios.post(API_URL + "/organizations/"+ orgId + "/applications", {
+            userId
+        }, {
+            headers: authHeader()
+        });
+        return response.data;
+    }
+
+    async createNewOrganization(data){
+        const response = await axios.post(API_URL + "/organizations", data, {
+            headers: authHeader()
+        });
+        return response.data;
+    }
+
 }
 
 export default new OrganizationService();
